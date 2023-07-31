27a13bfb2cf553674e30a4caac0f35b8963ff54a
dongod01 <63616945+dongod01@users.noreply.github.com>
2023-07-31 21:58:11 +0530

    Update mailsettings.py
    Changed pw location to local

--- mailsettings.py
+++ mailsettings.py
@@ -2,8 +2,13 @@ DEBUG = True
 MAIL_SERVER='smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USERNAME = 'firedetect.kgp@gmail.com'
-MAIL_PASSWORD = '^^kgp^^1'
-MAIL_PASSWORD = 'jxdyzrbhpdhkxury'
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_ASCII_ATTACHMENTS = True
+
+#updated July 23
+MAIL_PASSWORD = None
+
+with open('C:/Users/DELL/Desktop/Codes/Info/firedetect_info.txt') as file:
+        MAIL_PASSWORD = file.readlines()[1]
+        # Copying app pw from another dir
